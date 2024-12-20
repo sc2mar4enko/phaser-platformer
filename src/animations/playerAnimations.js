2920bb2aa49a20e33aca764c993f130af1c00056
Konstantin Marchenko <sc2mar4enko@gmail.com>
2024-12-20 19:31:36 +0300

    slide animation

--- src/animations/playerAnimations.js
+++ src/animations/playerAnimations.js
@@ -26,4 +26,11 @@ export default function initAnimations(anims) {
         frameRate: 14,
         repeat: 0
     });
+    
+    anims.create({
+        key: 'slide',
+        frames: anims.generateFrameNames('slide-sheet', {start: 0, end: 2}),
+        frameRate: 8,
+        repeat: 0
+    });
 }
